aa0b7f523d2518a1c2a3fe6be63095c0939f46d3
Ibnmardanis24 <ginessalar24@gmail.com>
2020-06-20 16:51:51 +0200

    Added initpop.py and varlimit.py, fixed matrix_mani.py

--- python_version/nsga_ii/initpop.py
+++ python_version/nsga_ii/initpop.py
@@ -1,2 +1,156 @@
-def initpop():
-    return 0
+def initpop(opt, pop, *varargin):
+# Function: pop = initpop(opt, pop, varargin)
+# Description: Initialize population.
+# Syntax:
+#   pop = initpop(opt, pop)
+#     (default) Create a random initial population with a uniform distribution.
+#
+#   pop = initpop(opt, pop, 'pop.txt')
+#     Load population from exist file and use the last population. If the popsize
+#     less than the current popsize, then random numbers will used to fill the population.
+#
+#   pop = initpop(opt, pop, 'pop.txt', ngen)
+#     Load population from file with specified generation.
+#
+#   pop = initpop(opt, pop, oldresult)
+#     Specify exist result structure.
+#
+#   pop = initpop(opt, pop, oldresult, ngen)
+#     Specify exist result structure and the population which will be used.
+#
+# Parameters:
+#   pop : an empty population
+#
+#*************************************************************************
+#*************************************************************************
+# 1. Identify parameters
+#*************************************************************************
+    method = 'uniform'
+    if (2 + len(varargin) >= 3):
+        if varargin[0] is str:
+            method = 'file'
+        elif varargin[0] is dict:
+            method = 'existpop'
+#*************************************************************************
+# 2. Initialize population with different methods
+#*************************************************************************
+    if method == 'uniform':
+        pop = initpopUniform(opt,pop)
+    elif method == 'file':
+        print('...Initialize population from file ' + varargin[0] + '.')
+        pop = initpopFromFile(opt, pop, varargin)
+    elif method == 'existpop':
+        print('...Initialize population from specified result.')
+        pop = initpopFromExistResult(opt, pop, varargin)
+    return pop
+
+def initpopFromFile(opt, pop, *varargin):
+# Function: pop = initpopFromFile(opt, pop, varargin)
+# Description: Load population from specified population file.
+# Syntax:
+#   pop = initpop(opt, pop, 'pop.txt')
+#   pop = initpop(opt, pop, 'pop.txt', ngen)
+#
+#*************************************************************************
+#
+# Import required functions
+#
+    from nsga_ii.loadpopfile import loadpopfile
+
+    fileName = varargin[0]
+
+    oldResult = loadpopfile(fileName)
+    pop = initpopFromExistResult(opt, pop, oldResult, varargin[1:])
+    return pop
+
+def initpopFromExistResult(opt, pop, *varargin):
+# Function: pop = initpopFromExistResult(opt, pop, varargin)
+# Description: Load population from exist result structure.
+# Syntax:
+#   pop = initpop(opt, pop, oldresult)
+#   pop = initpop(opt, pop, oldresult, ngen)
+#
+#*************************************************************************
+#
+# Import Warnings module for Python
+#
+    import warnings
+# 1. Verify param
+    oldresult = varargin[0]
+    try:
+        oldpops = oldresult['pops']
+    except:
+        raise Exception('NSGA2:InitPopError. The result structure specified is not correct!')
+
+    if opt['numVar'] != len(oldpops[0][0]['var']) or\
+        opt['numObj'] != len(oldpops[0][0]['obj']) or\
+        opt['numCons'] != len(oldpops[0][0]['cons']):
+        raise Exception('NSGA2:InitPopError.\
+            The specified optimization result is not for current optimization model!')
+
+# 2. Determine which population would be used
+    ngen = 0
+    if (2 + len(varargin) >= 4):
+        ngen = varargin[1]
+
+    maxGen = len(varargin)
+
+    if ngen == 0:
+        ngen = maxGen
+    elif ngen > maxGen:
+        warnings.warn(f'NSGA2:InitPopWarning.\
+            The specified generation "{ngen}" does not exist, using "{maxGen}" instead.')
+
+    ngen = maxGen
+
+# 3. Create initial population
+    popsizeOld = len(oldpops[0])
+    popsizeNew = opt['popsize']
+
+    if popsizeNew <= popsizeOld:    # a) All from old pop
+        for i in range(0, popsizeNew):
+            pop[i]['var'] = oldpops[ngen][i]['var']
+    else:                           # b) Use random individuals to fill the population
+        for i in range(0, popsizeOld):
+            pop[i]['var'] = oldpops[ngen][i]['var']
+        pop[popsizeOld:] = initpopUniform(opt, pop[popsizeOld:])
+    return pop
+
+def initpopUniform(opt, pop):
+# Function: pop = initpopUniform(opt, pop)
+# Description: Initialize population using random number
+#
+#*************************************************************************
+#
+# Import required functions
+#
+    from utilities.matrix_manipulation import rand_value_matrix, transpose, vector_multiply, matrix_subtraction
+    from nsga_ii.varlimit import varlimit
+
+    nVar = opt['numVar']
+    type = opt['vartype']
+
+    lb = opt['lb']
+    ub = opt['ub']
+
+    popsize = len(pop)
+    for i in range(0, popsize):
+        var1 = matrix_subtraction(ub, transpose(lb))
+        var = vector_multiply(rand_value_matrix(1, nVar), var1[0])
+        for j in range(len(var)):
+            for k in range(len(var[0])):
+                var[j][k] += lb[j][0]
+
+
+# if desing variable is integer, round to the nearest integer
+    print(var)
+    for j in range(len(var)):
+        for k in range(len(var[0])):
+            if( type[j] == 2):
+                var[j][k] = round(var[j][k])
+
+# limit in the lower and upper bound
+    var = varlimit(var, lb, ub)
+
+    pop[i]['var'] = var
+    return pop
--- python_version/utilities/matrix_manipulation.py
+++ python_version/utilities/matrix_manipulation.py
@@ -14,3 +14,139 @@ def value_matrix(rows, cols, value):
             M[-1].append(value)
 
     return M
+
+def rand_value_matrix(rows, cols):
+#    """
+#    Creates a matrix filled with the random value.
+#        :param rows: the number of rows the matrix should have
+#        :param cols: the number of columns the matrix should have
+
+#        :return: list of lists that form the matrix
+#    """
+    import random
+    M = []
+    if rows > 1:
+        while len(M) < rows:
+            M.append([])
+            while len(M[-1]) < cols:
+                M[-1].append(random.random())
+    else:
+        while len(M) < cols:
+            M.append(random.random())
+
+    return M
+
+def matrix_multiply(A, B):
+#    """
+#    Returns the product of the matrix A * B
+#        :param A: The first matrix - ORDER MATTERS!
+#        :param B: The second matrix
+
+#        :return: The product of the two matrices
+#    """
+    # Section 1: Ensure A & B dimensions are correct for multiplication
+    rowsA = len(A)
+    if isinstance(A[0],list):
+        colsA = len(A[0])
+    else:
+        colsA = 1
+    rowsB = len(B)
+    if isinstance(B[0],list):
+        colsB = len(B[0])
+    else:
+        colsB = 1
+    if colsA != rowsB:
+        raise ArithmeticError(
+            'Number of A columns must equal number of B rows.')
+
+    # Section 2: Store matrix multiplication in a new matrix
+    C = value_matrix(rowsA, colsB, 0)
+    for i in range(rowsA):
+        for j in range(colsB):
+            total = 0
+            for ii in range(colsA):
+                total += A[i][ii] * B[ii][j]
+            C[i][j] = total
+
+    return C
+
+def vector_multiply(A, B):
+#    """
+#    Returns the product of the vectors A * B
+#        :param A: The first row vector - ORDER MATTERS!
+#        :param B: The second row vector
+
+#        :return: The product of the two vectors (Matrix)
+#    """
+    # Section 1: Take A & B dimensions
+    colsA = len(A)
+    colsB = len(B)
+
+    # Section 2: Store matrix multiplication in a new matrix
+    C = value_matrix(colsA, colsB, 0)
+    for i in range(colsA):
+        for j in range(colsB):
+            C[i][j] = A[i]*B[j]
+
+    return C
+
+def transpose(M):
+#    """
+#    Returns a transpose of a matrix.
+#        :param M: The matrix to be transposed
+#
+#        :return: The transpose of the given matrix
+#    """
+    # Section 1: if a 1D array, convert to a 2D array = matrix
+    if not isinstance(M[0],list):
+        M = [M]
+
+    # Section 2: Get dimensions
+    rows = len(M)
+    cols = len(M[0])
+
+    # Section 3: MT is zeros matrix with transposed dimensions
+    MT = value_matrix(cols, rows, 0)
+
+    # Section 4: Copy values from M to it's transpose MT
+    for i in range(rows):
+        for j in range(cols):
+            MT[j][i] = M[i][j]
+
+    return MT
+
+def matrix_subtraction(A, B):
+#    """
+#    Subtracts matrix B from matrix A and returns difference
+#        :param A: The first matrix
+#        :param B: The second matrix
+
+#        :return: Matrix difference
+#    """
+    # Section 1: Ensure dimensions are valid for matrix subtraction
+    rowsA = len(A)
+    if isinstance(A[0],list):
+        colsA = len(A[0])
+    else:
+        colsA = 1
+    rowsB = len(B)
+    if isinstance(B[0],list):
+        colsB = len(B[0])
+    else:
+        colsB = 1
+    if rowsA != rowsB or colsA != colsB:
+        raise ArithmeticError('Matrices are NOT the same size.')
+
+    # Section 2: Create a new matrix for the matrix difference
+    C = value_matrix(rowsA, colsB, 0)
+
+    # Section 3: Perform element by element subtraction
+    if isinstance(A[0],list) and isinstance(B[0],list):
+        for i in range(rowsA):
+            for j in range(colsB):
+                C[i][j] = A[i][j] - B[i][j]
+    else:
+        for i in range(rowsA):
+            C[i] = A[i] - B[i]
+
+    return C
